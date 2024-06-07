af665dfebd890fe3afe24895a000610284ebab84
Brooke Richards <brichards@nwmissouri.edu>
2024-06-07 08:37:55 -0500

    okay now I'm really done messing around

--- sql/query_aggregation.sql
+++ sql/query_aggregation.sql
@@ -2,4 +2,7 @@
 SELECT COUNT(*) AS total_books,
        AVG(year_published) AS average_year_published,
        SUM(LENGTH(title)) AS total_characters_in_titles
+
+--calculate the average age of book
+SELECT AVG(year_published) AS average_publish_year
 FROM books;
